b43a766dc0bfee7b1482bc33008c630bfd93896c
Justin2Liu@taiwanmobile.com <Justin2Liu@taiwanmobile.com>
2015-04-01 23:56:33 +0800

    remove unnecessay import

--- projects/app/NetworkCommunication_step5/src/com/example/networkcommunication/MainActivity.java
+++ projects/app/NetworkCommunication_step5/src/com/example/networkcommunication/MainActivity.java
@@ -16,7 +16,6 @@ import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.android.volley.Request;
